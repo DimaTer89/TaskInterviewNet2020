114d96af89eb2fc6ad1fd7c645ec9547201f2533
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2020-07-05 14:18:47 +0300

    Update Program.cs

--- TaskEpam.Net2020/ClockConsole/Program.cs
+++ TaskEpam.Net2020/ClockConsole/Program.cs
@@ -14,39 +14,39 @@ namespace ClockConsole
             while (!isExit)
             {
                 Console.Clear();
-                Console.WriteLine("1 - Установить угол поворота часовой стрелки");
-                Console.WriteLine("2 - Установить угол поворота минутной стрелки");
-                Console.WriteLine("3 - Установить угол поворота секундной стрелки");
-                Console.WriteLine("4 - Показать цифровое время");
-                Console.WriteLine("5 - Выход");
-                Console.Write("Ваш выбор: ");
+                Console.WriteLine("1 - Set hour hand angle");
+                Console.WriteLine("2 - Set minute hand angle");
+                Console.WriteLine("3 - Set second hand angle");
+                Console.WriteLine("4 - Show digital time");
+                Console.WriteLine("5 - Exit");
+                Console.Write("Your choice: ");
                 try
                 {
                     choiceNumber = int.Parse(Console.ReadLine());
                     switch (choiceNumber)
                     {
                         case 1:
-                            Console.Write("Введите угол от 0 до 720: ");
+                            Console.Write("Set angle between 0 and 720: ");
                             clockAdapter.SetHourAngle(double.Parse(Console.ReadLine()));
                             break;
                         case 2:
-                            Console.Write("Введите угол от 0 до 360: ");
+                            Console.Write("Set angle between 0 and 360: ");
                             clockAdapter.SetMinuteAngle(double.Parse(Console.ReadLine()));
                             break;
                         case 3:
-                            Console.Write("Введите угол от 0 до 360: ");
+                            Console.Write("Set angle between 0 and 360: ");
                             clockAdapter.SetSecondAngle(double.Parse(Console.ReadLine()));
                             break;
                         case 4:
-                            Console.WriteLine($"Время в цифровом виде: {clockAdapter.ShowDigitalTime()}");
+                            Console.WriteLine($"Digital time: {clockAdapter.ShowDigitalTime()}");
                             Console.ReadKey();
                             break;
                         case 5:
-                            Console.WriteLine("До свидания!");
+                            Console.WriteLine("Goodbye!");
                             isExit = true;
                             break;
                         default:
-                            Console.WriteLine("Неверный выбор меню");
+                            Console.WriteLine("Incorrect menu select");
                             break;
                     }
                 }
